0a79cec356e537cbebe55bf5ab36c8eccaaa5e98
BezkorovainaYuliia <yuloiya.bezkorovayna@gmail.com>
2026-04-01 20:00:30 +0200

    add header and menu

---
+++ my-react/src/component/Header.jsx
@@ -0,0 +1,17 @@
+const Header = () => {
+    return(<>
+            <h1>
+                Header
+            </h1>
+        </>
+    );
+}
+
+export function print(value){
+    return <h2>Header 2 as sample</h2>
+}
+
+export function print2(value){
+    return <h3>Header 3 as sampple</h3>
+}
+export default Header;
---
+++ my-react/src/component/Menu.jsx
@@ -0,0 +1,19 @@
+const Menu = () =>{
+    return (
+        <div>
+            <ul>
+                <li>
+                    <a href="">Home</a>
+                </li>
+                <li>
+                    <a href="">Contact Us</a>
+                </li>
+                <li>
+                    <a href="">Privacy Policy</a>
+                </li>
+            </ul>
+        </div>
+    );
+}
+
+export default Menu;
